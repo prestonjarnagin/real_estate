aaa6e630115ca1c32153038037a8ae7999db1835
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-29 14:18:45 -0600

    Add test to room_test

--- test/room_test.rb
+++ test/room_test.rb
@@ -16,6 +16,11 @@ class RoomTest < Minitest::Test
     assert_equal :bedroom, @room.category
   end
 
+  def test_room_has_length_and_width
+    assert_equal 10, @room.length
+    assert_equal 13, @rrom.width
+  end
+
   def test_room_can_have_area
     assert_equal 130, @room.area
   end
