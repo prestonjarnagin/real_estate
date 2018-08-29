323baa9d56790c265a11b765a7b7bbc4420ad8ac
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-29 13:23:59 -0600

    Write house class. Fix house_test. Finish iteration 2.

--- test/house_test.rb
+++ test/house_test.rb
@@ -1,13 +1,13 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/houser'
+require './lib/house'
+require './lib/room'
 
 class HouseTest < Minitest::Test
 
   def setup
     @house = House.new("$400000", "123 sugar lane")
   end
-
   def test_it_exists
     assert_instance_of House, @house
   end
