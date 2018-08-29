a72f8cf5990433fb802158d9fc884425ec58afcf
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-29 13:08:42 -0600

    Write tests for iteration 1

--- test/room_test.rb
+++ test/room_test.rb
@@ -4,7 +4,20 @@ require './lib/room'
 
 class RoomTest < Minitest::Test
 
-  def test_case_name
-
+  def setup
+    @room = Room.new(:bedroom, 10, 13)
   end
+
+  def test_it_exists
+    assert_instance_of Room, @room
+  end
+
+  def test_room_can_have_category
+    assert_equal :bedroom, @room.caregory
+  end
+
+  def test_room_can_have_area
+    assert_equal 130, @room.area
+  end
+
 end
