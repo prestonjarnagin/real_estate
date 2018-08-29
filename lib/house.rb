b266a124f24cdc7a52da24cfd958f6b3eceeb7f9
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-29 13:38:25 -0600

    Update house. Finish iteration_3.

--- lib/house.rb
+++ lib/house.rb
@@ -14,4 +14,19 @@ class House
     @rooms << room
   end
 
+  def rooms_from_category(category)
+    rooms.find_all do |room|
+      room.category == category
+    end
+  end
+
+  def area
+    #Inject pattern?
+    x = 0
+    rooms.each do |room|
+      x += room.area
+    end
+    return x
+  end
+
 end
